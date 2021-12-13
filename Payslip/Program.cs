32af4aeb6c15372215d18e4c7b8f23ab6a61df48
Roya.T <32629566+Roya-myob@users.noreply.github.com>
2021-12-13 15:00:07 +1100

    TaxCalculator_Logic_And_Test

--- Payslip/Program.cs
+++ Payslip/Program.cs
@@ -6,7 +6,7 @@ namespace Payslip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
--- PayslipTests/EmployeeSuperTest.cs
+++ PayslipTests/EmployeeSuperTest.cs
@@ -6,12 +6,10 @@ namespace PayslipTests
     public class EmployeeSuperTest
     {
         [Fact]
-        public void PaySlip_Calculates_employee_super_for_given_super_rate_and_annual_salary()
+        public void PaySlip_Calculates_Super_For_Given_Super_Rate_And_Annual_Salary()
         {
             PayslipEngine payslipEngine = new PayslipEngine(new EmployeeSuper(new EmployeeAnnualSalary(60050),9));
-
             var actual = 450;
-            
             Assert.Equal(payslipEngine.EmployeeSuper(), actual);
 
 
